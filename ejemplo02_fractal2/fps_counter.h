d6b846fd7fbbe8d1a4a6337449fb229a27be3e34
Alulestad <vladimirlestad@gmail.com>
2024-11-29 05:19:03 -0500

    Actualizar .gitignore y limpiar archivos rastreados

--- ejemplo02_fractal2/fps_counter.h
+++ ejemplo02_fractal2/fps_counter.h
@@ -14,6 +14,7 @@
 namespace ch = std::chrono; // Alias para simplificar el uso de std::chrono.
 
 //
+//
 class fps_counter {
 private:
     int frames; // Contador de cuadros procesados desde el último cálculo de FPS.
